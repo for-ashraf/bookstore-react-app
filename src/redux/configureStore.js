bb9c2489a65f6296c0b2066442cb1d600c09bc6b
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-16 14:35:12 +0500

    Store Configuration
    Store configuration with reducers registration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,12 @@
-const store = configureStore({ reducer: counterReducer })
+import { combineReducers, configureStore } from 'redux';
+import booksReducer from './books.reducer';
+import categoriesReducer from './categories.reducer';
+
+const appReducer = combineReducers({
+  booksReducer,
+  categoriesReducer,
+});
+
+const bookStore = configureStore(appReducer);
+
+export default bookStore;
