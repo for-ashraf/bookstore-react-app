80f09f6922e009e59c5b9e68327869f85baabaa3
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-20 01:13:11 +0500

    required changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,28 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import userIcon from './images/user.png';
 
-const links = [
-  {
-    id: 1,
-    path: '/',
-    text: 'BOOKS',
-  },
-  {
-    id: 2,
-    path: '/categories',
-    text: 'CATEGORIES',
-  },
-];
-function Navbar() {
-  return (
-    <div className="navContainer">
-      <nav className="navbar">
-        <h1 className="mainTitle">Education Soul</h1>
-        <ul className="navBarUl">
-          {links.map((link) => (
-            <li key={link.id} className={link.text}>
-              <Link to={link.path}>{link.text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+const Navbar = () => (
+  <nav className="navigation-bar">
+    <div className="nav-div1">
+      <span className="nav-logo">EducationSoul</span>
+      <ul className="nav-ul">
+        <li>
+          <NavLink to="/" className="bookstore-link">
+            Bookstore
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="categories" className="categories-link">
+            Categories
+          </NavLink>
+        </li>
+      </ul>
     </div>
-  );
-}
+    <div className="nav-div2">
+      <img className="nav-user-icon" src={userIcon} alt="user-icon" />
+    </div>
+  </nav>
+);
 
 export default Navbar;
